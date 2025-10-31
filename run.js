2e58c6619e4c05dba62e366abe16fb1b7f4548eb
Inam Ullah <inamu0448@gmail.com>
2025-10-31 09:55:26 +0500

    adding the lock file

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 const satellite = require("./src/satellite");
 const iridium = require("./src/iridium");
-
+/////////////////////////////////Hello//////////
 var location = [39.9042, 116.4074, "%E5%8C%97%E4%BA%AC%E5%B8%82", 52, "ChST"];
 //COOKIE需要先通过浏览器调到中文
 
